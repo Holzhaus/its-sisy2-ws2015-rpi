1f8f86d6a64fd74c008e3acca9abd333d11d4f3c
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2016-01-14 17:29:45 +0100

    Put main code into dedicated function

--- main.py
+++ main.py
@@ -6,37 +6,47 @@ from lib import sensor
 from lib import server
 from lib import client
 
-parser = argparse.ArgumentParser(
-    description='SiSy2 Gyro Project',
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('--debug', action='store_true', help='Show debug messages')
-parser.add_argument('--host', metavar='HOST', nargs=1,
-                    action='store', help='Server IP', default='localhost')
-parser.add_argument('--port', metavar='PORT', type=int, nargs=1,
-                    action='store', help='Server port', default=9876)
-parser.add_argument('--auth', action='store_true', help='Use auth protocol')
-mode = parser.add_mutually_exclusive_group(required=True)
-mode.add_argument('--client', action='store_true', help='Run as client',
-                  default=True)
-mode.add_argument('--server', action='store_true', help='Run as server')
-args = parser.parse_args()
 
-logging.basicConfig(level=logging.DEBUG if args.debug else logging.INFO)
+def main(args=None):
+    parser = argparse.ArgumentParser(
+        description='SiSy2 Gyro Project',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--debug', action='store_true',
+                        help='Show debug messages')
+    parser.add_argument('--host', metavar='HOST', nargs=1,
+                        action='store', help='Server IP', default='localhost')
+    parser.add_argument('--port', metavar='PORT', type=int, nargs=1,
+                        action='store', help='Server port', default=9876)
+    parser.add_argument('--auth', action='store_true',
+                        help='Use auth protocol')
+    mode = parser.add_mutually_exclusive_group(required=True)
+    mode.add_argument('--client', action='store_true', help='Run as client',
+                      default=True)
+    mode.add_argument('--server', action='store_true', help='Run as server')
+    p_args = parser.parse_args(args)
 
-print('Mode: %s' % ('Server' if args.server else 'Client'))
-print('Host: %s' % args.host)
-print('Port: %d' % args.port)
+    logging.basicConfig(level=logging.DEBUG if p_args.debug else logging.INFO)
 
-if args.server:
-    sensor_instance = sensor.Sensor()
-    server_class = server.AuthServer if args.auth else server.SimpleServer
-    server_instance = server_class(sensor_instance, args.host, args.port)
-    server_instance.run()
-elif args.client:
-    if args.auth:
+    print('Mode: %s' % ('Server' if p_args.server else 'Client'))
+    print('Host: %s' % p_args.host)
+    print('Port: %d' % p_args.port)
+
+    if p_args.server:
         sensor_instance = sensor.Sensor()
-        client_instance = client.AuthClient(
-            sensor_instance, args.host, args.port)
-    else:
-        client_instance = client.Client(args.host, args.port)
-    client_instance.run()
+        server_class = (server.AuthServer if p_args.auth
+                        else server.SimpleServer)
+        server_instance = server_class(sensor_instance,
+                                       p_args.host, p_args.port)
+        server_instance.run()
+    elif p_args.client:
+        if p_args.auth:
+            sensor_instance = sensor.Sensor()
+            client_instance = client.AuthClient(
+                sensor_instance, p_args.host, p_args.port)
+        else:
+            client_instance = client.Client(p_args.host, p_args.port)
+        client_instance.run()
+
+
+if __name__ == '__main__':
+    main()
