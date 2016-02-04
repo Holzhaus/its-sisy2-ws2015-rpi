5fffa8f194e9a48ff77e296619c95070a446f144
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2016-02-04 18:29:32 +0100

    main: Improve argument parsing

--- accelauth/__main__.py
+++ accelauth/__main__.py
@@ -10,31 +10,33 @@ def main(args=None):
     parser = argparse.ArgumentParser(
         description='SiSy2 Gyro Project',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--debug', action='store_true',
-                        help='Show debug messages')
-    parser.add_argument('--host', metavar='HOST', type=str,
+    parser.add_argument('-v', '--verbose', action='count', default=0,
+                        help="Be verbose")
+    parser.add_argument('-H', '--host', metavar='HOST', type=str,
                         action='store', help='Server IP', default='localhost')
-    parser.add_argument('--port', metavar='PORT', type=int,
+    parser.add_argument('-S', '--port', metavar='PORT', type=int,
                         action='store', help='Server port', default=9876)
-    parser.add_argument('--auth', action='store_true',
+    parser.add_argument('-a', '--auth', action='store_true',
                         help='Use auth protocol')
-    parser.add_argument('--bus', metavar='BUS', type=int,
+    parser.add_argument('-b', '--bus', metavar='BUS', type=int,
                         action='store', help='SMBus ID', default=1)
-    parser.add_argument('--offset', action='store', type=int, nargs=3,
+    parser.add_argument('-o', '--offset', action='store', type=int, nargs=3,
                         metavar=('X', 'Y', 'Z'), default=(0, 0, 0),
                         help='Sensor offset')
-    parser.add_argument('--no-quantization', action='store_true',
+    parser.add_argument('-n', '--no-quantization', action='store_true',
                         help='Disable Quantization')
-    parser.add_argument('--demo', action='store_true', help='Use demo sensor')
+    parser.add_argument('-x', '--demo', action='store_true',
+                        help='Use demo sensor')
     mode = parser.add_mutually_exclusive_group(required=True)
-    mode.add_argument('--client', action='store_true', help='Run as client',
-                      default=True)
-    mode.add_argument('--server', action='store_true', help='Run as server')
-    mode.add_argument('--test-sensor', action='store_true',
+    mode.add_argument('-c', '--client', action='store_true',
+                      default=True, help='Run as client')
+    mode.add_argument('-s', '--server', action='store_true',
+                      help='Run as server')
+    mode.add_argument('-t', '--test-sensor', action='store_true',
                       help='Test the sensor by printing the values')
     p_args = parser.parse_args(args)
 
-    logging.basicConfig(level=logging.DEBUG if p_args.debug else logging.INFO)
+    logging.basicConfig(level=max([0, 30-p_args.verbose*10]))
 
     print('Demo sensor: %s' % ('Yes' if p_args.demo else
                                'No (SMBus ID %d)' % p_args.bus))
