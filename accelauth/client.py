b9ea9d1f532d9670914d34273f910ceeb6ee5a69
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2016-02-04 17:55:18 +0100

    client/server: improve logging massively

--- accelauth/client.py
+++ accelauth/client.py
@@ -72,38 +72,48 @@ class AuthClient(Client):
         self._logger.info("Client exited.")
 
     def key_agreement(self, secret):
-
-        self._logger.debug('Trying to authenticate...')
+        self._logger.info('Trying to authenticate...')
+        self._logger.debug('secret = %r', secret)
 
         # Generate challenge cc and send to server
+        self._logger.info('Generating client challenge...')
         cc = protocol.get_random_bytes()
-        self._logger.debug('Generated cc = %r', cc)
+        self._logger.debug('cc = %r', cc)
 
+        self._logger.info('Sendig client challenge to server...')
         sc, sr = (self._server.start_challenge_response(Binary(cc)))
         sc = sc.data
-
-        print(sc)
+        self._logger.info('Received server challenge/response...')
+        self._logger.debug('sc = %r', sc)
 
         if cc == sc:
-            self._logger.debug('cc == sc, aborting')
+            self._logger.warning('Both challenges are the same, aborting...')
             return
 
         # Check if server response is valid
+        self._logger.info('Checking if server response is valid...')
         sr_expected = protocol.generate_response(sc, cc, secret)
+        self._logger.debug('sr1 = %r', sr)
+        self._logger.debug('sr2 = %r (expected)', sr_expected)
 
         if sr == sr_expected:
-            self._logger.debug('sr == sr_expected')
             # Server response is valid, we'll send him the client response
+            self._logger.info('Server response is valid!')
             cr = protocol.generate_response(cc, sc, secret)
         else:
-            self._logger.debug('sr != sr_expected = %r', sr)
             # Server response is invalid, we'll send bogus to the server
+            self._logger.warning('Server response is invalid!')
             cr = protocol.generate_response(protocol.get_random_bytes())
+        self._logger.debug('cr = %r', cr)
 
+        self._logger.info('Sending client response....')
         self._server.finish_challenge_response(cr)
 
         if sr == sr_expected:
+            self._logger.info('Generating key...')
             key = protocol.generate_key(secret, cc, sc)
+            self._logger.info('Key generated!')
+            self._logger.debug('key = %r', key)
             return key
         else:
             return None
@@ -116,12 +126,20 @@ class AuthClient(Client):
             if plaintext.lower() == 'logout':
                 stopped = True
 
+            # Encrypt our message
             iv, ciphertext = protocol.encrypt(key, plaintext)
+            self._logger.debug('iv = %r', iv)
+            self._logger.debug('ciphertext = %r', ciphertext)
 
+            # Send it to the server
+            self._logger.info('Sending message to server...')
             s_iv, s_ciphertext = (
                 x.data if isinstance(x, Binary) else x
                 for x in self._server.send_message(
                     Binary(iv), Binary(ciphertext)))
 
+            # Decrypt the server's message
+            self._logger.debug('s_iv = %r', s_iv)
+            self._logger.debug('s_ciphertext = %r', s_ciphertext)
             s_message = protocol.decrypt(key, s_iv, s_ciphertext)
             print('SERVER: %s' % s_message)
--- accelauth/server.py
+++ accelauth/server.py
@@ -118,20 +118,25 @@ class AuthServer(Server):
         self._logger.info("Server exited.")
 
     def start_challenge_response(self, uid, cc):
+        self._logger.info('New authentication request from %r!', uid)
         cc = cc.data
         secret = self._sensor.get_rotation()
+        self._logger.debug('secret = %r', secret)
+        self._logger.debug('cc = %r', cc)
 
+        self._logger.info('Generating server challenge...')
         sc = protocol.get_random_bytes()
         while sc == cc:  # Make sure we don't use the same challenge
             sc = protocol.get_random_bytes()
+        self._logger.debug('sc = %r', sc)
 
-        self._logger.debug('Generated sc = %r', sc)
-
+        self._logger.info('Generating server response...')
         sr = protocol.generate_response(sc, cc, secret)
-
-        self._logger.debug('Generated sr')
+        self._logger.debug('sr = %r', sr)
 
         self._userdata[uid] = (cc, sc, secret)
+        self._logger.info('Sending server challenge/response to %r and ' +
+                          'wait...', uid)
 
         return (Binary(sc), sr)
 
@@ -141,18 +146,25 @@ class AuthServer(Server):
         except KeyError:
             self._logger.warning('Invalid connection attempt from %r', uid)
         else:
+            self._logger.info('Continuing authentication with %r!', uid)
             (cc, sc, secret) = data
 
             # Check if client response is valid
+            self._logger.info('Checking if client response is valid...')
             cr_expected = protocol.generate_response(cc, sc, secret)
+            self._logger.debug('cr1 = %r', cr)
+            self._logger.debug('cr2 = %r (expected)', cr_expected)
 
             if cr == cr_expected:
                 # Client response is valid, let's generate the key
+                self._logger.info('Client response is valid!')
+                self._logger.info('Generating key...')
                 key = protocol.generate_key(secret, cc, sc)
                 self._userkeys[uid] = key
-                self._logger.debug('cr == cr_expected, we have a key!')
+                self._logger.info('Key generated!')
+                self._logger.debug('key = %r', key)
             else:
-                self._logger.debug('cr != cr_expected, no key generated')
+                self._logger.warning('Client response is invalid!')
         return "ok"
 
     def send_message(self, uid, c_iv, c_ciphertext):
@@ -165,6 +177,9 @@ class AuthServer(Server):
             x.data if isinstance(x, Binary) else x
             for x in (c_iv, c_ciphertext))
 
+        # Decrypt the client's message
+        self._logger.debug('c_iv = %r', c_iv)
+        self._logger.debug('c_ciphertext = %r', c_ciphertext)
         c_message = protocol.decrypt(key, c_iv, c_ciphertext)
 
         print("Received message: %r" % c_message)
@@ -177,5 +192,10 @@ class AuthServer(Server):
             s_message = 'Goodbye!'
         else:
             s_message = 'I received your message, thanks!'
+
+        # Encrypt the server's answer and send it to the client
+        self._logger.debug('s_message = %r', s_message)
         s_iv, s_ciphertext = protocol.encrypt(key, s_message)
+        self._logger.debug('s_iv = %r', s_iv)
+        self._logger.debug('s_ciphertext = %r', s_ciphertext)
         return (Binary(s_iv), Binary(s_ciphertext))
